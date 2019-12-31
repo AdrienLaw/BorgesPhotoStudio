5cbdd3bdb1152cbda5b26acad0031fb5d6ef500c
luohaotian <up842514938>
2019-12-31 12:24:24 +0800

    git config client origin

---
+++ eureka_config_client/src/main/java/com/adrien/EurekaConfigClientApplication.java
@@ -0,0 +1,15 @@
+package com.adrien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class EurekaConfigClientApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(EurekaConfigClientApplication.class,args);
+    }
+}
