ae779b58746e49e8665b636fb4248efbce4013ca
luohaotian <up842514938>
2020-01-01 14:01:35 +0800

    配置中心服务化和高可用

---
+++ eureka_configuration_client/src/main/java/com/adrien/EurekaConfigurationClientApplication.java
@@ -0,0 +1,17 @@
+package com.adrien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaConfigurationClientApplication {
+    public static void main( String[] args ) {
+        SpringApplication.run(EurekaConfigurationClientApplication.class,args);
+    }
+}
---
+++ eureka_configuration_server2/src/main/java/com/adrien/EurekaConfigurationServerApplication02.java
@@ -0,0 +1,15 @@
+package com.adrien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class EurekaConfigurationServerApplication02 {
+    public static void main( String[] args ) {
+        SpringApplication.run(EurekaConfigurationServerApplication02.class,args);
+    }
+}
